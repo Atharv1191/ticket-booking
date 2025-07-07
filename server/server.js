bb73c724a7de3aca808940a77aa7b887e8ae95b5
Atharv Joshi <atharvj07@gmail.com>
2025-07-07 11:49:15 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -18,6 +18,6 @@ app.use(cors())
 app.use(clerkMiddleware())
 
 app.get('/',(req,res)=>res.send("server is Live"))
-app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/inngest", serve({ inngest, functions }));
 
 app.listen(port,()=>console.log(`Server running on port ${port}`))
